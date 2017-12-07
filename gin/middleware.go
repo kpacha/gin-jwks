c165bb500d51863facdd0f34120bf27744fd11b5
kpacha <kpacha666@gmail.com>
2017-12-07 01:50:55 +0100

    abstract verify middleware added

--- gin/middleware.go
+++ gin/middleware.go
@@ -28,9 +28,8 @@ func ToHTTPContext() gin.HandlerFunc { return ToHTTPContextWithKey(JWTTokenConte
 // in the request context, using the injected key
 func ToHTTPContextWithKey(tokenKey string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tok := c.Request.Header.Get("Authorization")
-		if len(tok) > 6 && strings.ToUpper(tok[0:7]) == "BEARER " {
-			c.Set(tokenKey, []byte(tok[7:]))
+		if tok, ok := AuthHeaderTokenExtractor(c); ok {
+			c.Set(tokenKey, tok)
 		}
 		c.Next()
 	}
@@ -45,24 +44,66 @@ func Auth(verifier jwks.Verifier) gin.HandlerFunc {
 // AuthWithKeys is a gin middleware that extracts the JWT from the injected context location, verifies it
 // and stores its claims in the request context, using the received key
 func AuthWithKeys(verifier jwks.Verifier, tokenKey, claimsKey string) gin.HandlerFunc {
+	return Verify(VerifyCfg{
+		Verifier:       verifier,
+		TokenExtractor: ContextTokenExtractor(tokenKey),
+		ClaimsKey:      claimsKey,
+	})
+}
+
+// VerifyCfg defines a struct grouping all the params required for building the Verify middleware
+type VerifyCfg struct {
+	Verifier       jwks.Verifier
+	TokenExtractor TokenExtractor
+	ClaimsKey      string
+}
+
+// Verify creates a gin middleware that extracts the JWT with the injected token extractor, verifies it
+// and stores its claims in the request context, using the defined claims key
+func Verify(cfg VerifyCfg) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tok, ok := c.Get(tokenKey)
-		if !ok {
-			c.AbortWithError(http.StatusUnauthorized, ErrUnableToExtractToken)
-			return
-		}
-		token, ok := tok.([]byte)
-		if !ok {
-			c.AbortWithError(http.StatusUnauthorized, ErrUnableToExtractToken)
-			return
-		}
-		claims := jwks.Claims{}
-		if err := verifier(jwks.Token(token), &claims); err != nil {
+		claims, err := extractVerifiedClaims(c, cfg.TokenExtractor, cfg.Verifier)
+		if err != nil {
 			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
 
-		c.Set(claimsKey, claims)
+		c.Set(cfg.ClaimsKey, *claims)
 		c.Next()
 	}
+}
+
+// TokenExtractor defines the interface to implement for the token extractor functions
+type TokenExtractor func(*gin.Context) ([]byte, bool)
+
+// ContextTokenExtractor returns a TokenExtractor that gets the token from the request context
+func ContextTokenExtractor(tokenKey string) TokenExtractor {
+	return func(c *gin.Context) (token []byte, ok bool) {
+		tok, ok := c.Get(tokenKey)
+		if !ok {
+			return
+		}
+		token, ok = tok.([]byte)
+		return
+	}
+}
+
+// AuthHeaderTokenExtractor is a TokenExtractor that gets the token from the request Authorization header
+func AuthHeaderTokenExtractor(c *gin.Context) (token []byte, ok bool) {
+	tok := c.Request.Header.Get("Authorization")
+	if len(tok) > 6 && strings.ToUpper(tok[0:7]) == "BEARER " {
+		token = []byte(tok[7:])
+		ok = true
+	}
+	return
+}
+
+func extractVerifiedClaims(c *gin.Context, te TokenExtractor, verifier jwks.Verifier) (*jwks.Claims, error) {
+	token, ok := te(c)
+	if !ok {
+		return nil, ErrUnableToExtractToken
+	}
+	claims := &jwks.Claims{}
+	err := verifier(jwks.Token(token), claims)
+	return claims, err
 }
--- gin/middleware_test.go
+++ gin/middleware_test.go
@@ -6,11 +6,14 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lestrrat/go-jwx/jwa"
+	"github.com/lestrrat/go-jwx/jws"
 
 	"github.com/kpacha/gin-jwks"
 )
 
 func TestToHTTPContext(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	router := gin.New()
 	router.Use(ToHTTPContext())
 	router.GET("/", func(c *gin.Context) {
@@ -40,7 +43,7 @@ func TestToHTTPContext(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	expected := "{\"alive\":true}\n"
+	expected := "{\"alive\":true}"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
@@ -49,6 +52,7 @@ func TestToHTTPContext(t *testing.T) {
 }
 
 func TestAuth_noTokenInContext(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	router := gin.New()
 	router.Use(Auth(jwks.NoopVerifier))
 	router.GET("/", func(c *gin.Context) {
@@ -91,6 +95,7 @@ func TestAuth_noToken(t *testing.T) {
 }
 
 func TestAuth_wrongToken(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	router := gin.New()
 	router.Use(func(c *gin.Context) {
 		c.Set(JWTTokenContextKey, jwks.Token("token"))
@@ -115,13 +120,8 @@ func TestAuth_wrongToken(t *testing.T) {
 
 func TestAuth_ok(t *testing.T) {
 	token := "something"
-	router := gin.New()
-	router.Use(func(c *gin.Context) {
-		c.Set(JWTTokenContextKey, []byte(token))
-		c.Next()
-	})
 	expectedClaims := jwks.Claims{"aaa": "aaa"}
-	router.Use(Auth(func(tok jwks.Token, claims *jwks.Claims) error {
+	verifier := func(tok jwks.Token, claims *jwks.Claims) error {
 		if string(tok) != token {
 			t.Errorf("the token passed by the mdw wasn't expect: got %s want %s",
 				string(tok), token)
@@ -129,16 +129,25 @@ func TestAuth_ok(t *testing.T) {
 		}
 		*claims = expectedClaims
 		return nil
-	}))
+	}
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(func(c *gin.Context) {
+		c.Set(JWTTokenContextKey, []byte(token))
+		c.Next()
+	})
+
+	router.Use(Auth(verifier))
 	router.GET("/", func(c *gin.Context) {
 		cl, ok := c.Get(JWTClaimsContextKey)
 		if !ok {
-			t.Error("The token is not in the context")
+			t.Error("claims are not in the context")
 			return
 		}
 		claims, ok := cl.(jwks.Claims)
 		if !ok {
-			t.Error("The stored data is not a token")
+			t.Error("The stored claims are not claims")
 			return
 		}
 		if len(claims) != len(expectedClaims) {
@@ -148,7 +157,7 @@ func TestAuth_ok(t *testing.T) {
 	})
 
 	req, _ := http.NewRequest("GET", "/", nil)
-	req.Header.Add("Authorization", "BEARER something")
+	req.Header.Add("Authorization", "BEARER "+token)
 
 	rr := httptest.NewRecorder()
 	router.ServeHTTP(rr, req)
@@ -157,10 +166,103 @@ func TestAuth_ok(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	expected := "{\"alive\":true}\n"
+	expected := "{\"alive\":true}"
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
 	}
 
+}
+
+func TestVerify_noToken(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(Verify(VerifyCfg{
+		Verifier:       jwks.ErrorVerifier,
+		ClaimsKey:      "none",
+		TokenExtractor: AuthHeaderTokenExtractor,
+	}))
+	router.GET("/", func(c *gin.Context) {
+		t.Error("This handler shouldn't be called")
+		c.AbortWithStatus(http.StatusInternalServerError)
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+}
+
+func BenchmarkVerify_okHS256(b *testing.B) {
+	secret := []byte("secret")
+	payload := []byte(`{"iss":"http://example.com/", "exp": 1515606371}`)
+	token, err := jws.Sign(payload, jwa.HS256, secret)
+	if err != nil {
+		b.Error("Signature generated. got:", err.Error())
+		return
+	}
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(Verify(VerifyCfg{
+		Verifier:       jwks.HS256Verifier(secret, "http://example.com/"),
+		ClaimsKey:      "none",
+		TokenExtractor: AuthHeaderTokenExtractor,
+	}))
+	router.GET("/", func(c *gin.Context) {
+		c.Status(200)
+	})
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Set("Authorization", "bearer "+string(token))
+		router.ServeHTTP(w, req)
+		if w.Result().StatusCode != 200 {
+			b.Error("wrong status code:", w.Result().StatusCode)
+		}
+	}
+}
+
+func BenchmarkVerify_koExpiredHS256(b *testing.B) {
+	secret := []byte("secret")
+	payload := []byte(`{"iss":"http://example.com/", "exp": 1515}`)
+	token, err := jws.Sign(payload, jwa.HS256, secret)
+	if err != nil {
+		b.Error("Signature generated. got:", err.Error())
+		return
+	}
+
+	gin.SetMode(gin.TestMode)
+	router := gin.New()
+	router.Use(Verify(VerifyCfg{
+		Verifier:       jwks.HS256Verifier(secret, "http://example.com/"),
+		ClaimsKey:      "none",
+		TokenExtractor: AuthHeaderTokenExtractor,
+	}))
+	router.GET("/", func(c *gin.Context) {
+		b.Error("the handler shouldn't be executed")
+		c.Status(200)
+	})
+
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Set("Authorization", "bearer "+string(token))
+		router.ServeHTTP(w, req)
+		if w.Result().StatusCode == 200 {
+			b.Error("wrong status code:", w.Result().StatusCode)
+		}
+	}
 }
